1df7e80ed1e098314c20f4d1e221a3a15a173ce6
Basudev kumar <basudevkumar.india2019@gmail.com>
2025-08-24 20:18:53 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ const PORT = process.env.PORT || 5001;
 
 
 
-
+// 'http://localhost:5173'
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
